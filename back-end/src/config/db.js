58b3306f715e3142ba5812e09ac974f078595a3d
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-05-21 19:33:34 -0400

    ✨feat: adicionado nova colloction

--- back-end/src/config/db.js
+++ back-end/src/config/db.js
@@ -11,15 +11,25 @@ const client = new MongoClient(uri);
 //função de ligacão da conecção com o mongodb
 export async function conecta() {
   await client.connect();
+  console.log(`
+    conecto
+    `);
+
 }
 
 //função de desligar a conecção com o mongodb
 //assim fica mais rapido
 export async function fecha() {
   await client.close();
+  console.log(`
+    fecha
+    `);
+
 }
+
 //constante de cada colocação
 export const collUser = client.db(process.env.DB_1).collection(process.env.COLL_1);
 export const collColocacoes = client.db(process.env.DB_1).collection(process.env.COLL_2);
 export const collFavorito = client.db(process.env.DB_1).collection(process.env.COLL_3);
 export const collHistorico = client.db(process.env.DB_1).collection(process.env.COLL_4);
+export const collTopBusca = client.db(process.env.DB_1).collection(process.env.COLL_5);
