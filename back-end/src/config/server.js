cf8e2ca97508ceb014c33112c8da622bad56f286
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-05-21 19:34:46 -0400

    ♻️refactor: Adicionado a conexão ao cluster

--- back-end/src/config/server.js
+++ back-end/src/config/server.js
@@ -1,11 +1,13 @@
 //importação
 import { app }from "../app.js";
 import dotenv from "dotenv";
+import { conecta } from "./db.js";
 //conecção com o .env
 dotenv.config()
 //atribuindo valor as constantes
 const port = process.env.PORTA;
 
+await conecta();
 //roda o servidor deacordo com a porta
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
