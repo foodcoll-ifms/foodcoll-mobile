0b7d17507fa3fde86d6952a8dae58417fb4a90ae
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-05-21 20:07:16 -0400

    ✨feat: adicionado model do serviço termo

---
+++ back-end/src/modules/termo/termoModel.js
@@ -0,0 +1,18 @@
+import { collColocacoes } from "../../config/db.js";
+
+export async function procuaraColocacao(id) {
+  return collColocacoes
+  .find({_id: id})
+  .project({colocacao: 1, _id:0})
+  .toArray();
+}
+
+export async function detalheTermo(colocacao) {
+  console.log(`model detalhe`);
+
+  const resultado = collColocacoes.find({colocacao: colocacao}).project({_id: 0, imagem: 0});
+
+  for await (const doc of resultado) {
+    return doc;
+  }
+}
