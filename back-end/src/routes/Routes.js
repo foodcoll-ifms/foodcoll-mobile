41a3ca3773fe3fdccc5bc359234574ab96991849
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-05-21 20:00:49 -0400

    ✨feat: adicionado novas rotas de serviços

--- back-end/src/routes/Routes.js
+++ back-end/src/routes/Routes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { pesquisa } from "../controllers/pesquisaController.js";
-import { favorito } from "../controllers/favoritoController.js";
+import { pesquisa } from "../modules/pesquisa/pesquisaController.js";
+import { recomentacao, termoCompleto } from "../modules/termo/termoControler.js";
+import { favoritoArmazena, favoritoMostra } from "../modules/favorito/favoritoController.js";
+import { topBuscas } from "../modules/topBusca/topBuscaController.js";
 
 const router= express.Router();
 
 router.get('/search', pesquisa);
-router.get('/favorito/:ID', favorito)
+router.post('/favorito/:termo', favoritoArmazena);
+router.get('/favorito', favoritoMostra);
+router.get('/topBuscas', topBuscas);
+router.get('/detalheTermos/:termo', termoCompleto);
+router.get('/recomendacao/:termo', recomentacao);
 
 export { router };
