be7be5ea17b4d5d041171e28e7766fbe1561c432
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-05-21 20:06:29 -0400

    ✨feat: adicionado controller do serviço do termo

---
+++ back-end/src/modules/termo/termoController.js
@@ -0,0 +1,25 @@
+import { detalheTermo } from "./termoModel";
+
+export async function termoCompleto(req, res, next) {
+  const termo = req.params.termo;
+  console.log(`completo`);
+  console.log(req.params.termo);
+
+  try {
+    const resultado = await detalheTermo(termo);
+    res.json(resultado);
+  } catch (err) {
+    next(err)
+  }
+}
+
+export async function recomentacao(req, res, next) {
+  const termo = req.params.termo;
+
+  try {
+    const resultado = await detalheTermo(termo);
+    res.json(resultado.remissivas);
+  } catch (err) {
+    next(err)
+  }
+}
