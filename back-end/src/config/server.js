905078d0388791a7915d59e43549634cd00684f8
Rhafaely Ananda Costa dos Reis <rhafaelyreis@gmail.com>
2026-06-01 17:37:58 -0400

    🛠️chore: configura scripts e porta para deploy no Railway

--- back-end/src/config/server.js
+++ back-end/src/config/server.js
@@ -2,12 +2,15 @@
 import { app }from "../app.js";
 import dotenv from "dotenv";
 import { conecta } from "./db.js";
-//conecção com o .env
+
+//conexão com o .env
 dotenv.config()
+
 //atribuindo valor as constantes
-const port = process.env.PORTA;
+const port = process.env.PORT || process.env.PORTA || 3000;
 
 await conecta();
+
 //roda o servidor deacordo com a porta
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
