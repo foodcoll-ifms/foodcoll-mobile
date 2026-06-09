5afc7919e77deafe32163b1977200f9022030d42
Rhafaely Ananda Costa dos Reis <rhafaelyreis@gmail.com>
2026-06-09 15:48:31 -0400

    🐛fix: corrige importação

--- back-end/src/routes/Routes.js
+++ back-end/src/routes/Routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { pesquisa } from "../modules/pesquisa/pesquisaController.js";
-import { recomentacao, termoCompleto } from "../modules/termo/termoController.js";
+import { recomendacao, termoCompleto } from "../modules/termo/termoController.js";
 import { favoritoArmazena, favoritoMostra } from "../modules/favorito/favoritoController.js";
 import { topBuscas } from "../modules/topBusca/topBuscaController.js";
 import { glossario } from "../modules/glossario/glossarioController.js";
@@ -12,7 +12,7 @@ router.post('/favorito/:termo', favoritoArmazena);
 router.get('/favorito', favoritoMostra);
 router.get('/topBuscas', topBuscas);
 router.get('/detalheTermos/:termo', termoCompleto);
-router.get('/recomendacao/:termo', recomentacao);
+router.get('/recomendacao/:termo', recomendacao);
 router.get('/glossario', glossario);
 
 export { router };
