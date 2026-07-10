a4582f3a525d0e199c26ba7085d0a52598c336d3
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-07-10 09:37:05 -0400

    ♻️refactor: Foi corrigido a chamado para o banco de dados e adicionado comentários
    Renamed function and improved comments for clarity.

--- back-end/src/modules/topBusca/topBuscaModel.js
+++ back-end/src/modules/topBusca/topBuscaModel.js
@@ -1,23 +1,31 @@
 import { collTopBusca } from "../../config/db.js";
 
-export async function mostraTopCincoBuscas(limite) {
+// função para mostrar o top Buscas com parametro de limite
+export async function mostraTopBuscas(limite) {
+  // return com os resultado da busca na tabela de top Busca
+  // sort informa como será ordenado o resultado
+  // limit o limite de documentos
+  // project com o que não dever ser retornado
   return await collTopBusca.find().sort( {frequencia: -1 }).limit(limite).project({_id:0, termo:1}).toArray();
 }
 
+// função para armazenar dados novos as topBuscas
 export async function armazenaTopBuscas(termo) {
-  try {
-    console.log(`no modal do top`);
 
-    await collTopBusca.insertOne({
-          termo: termo,
-          frequencia : 1 }
-    );
-  } catch (err) {
-    console.log(`deu erro`);
+  console.log(`no modal da função armazenaTopBuscas`);
 
-    await collTopBusca.updateOne(
-        {termo: termo},
-        {$inc : { frequencia : 1 }},
-    );
-  }
+  // insertOne para inserir novos dados a tabela
+  // já possu uma comparação
+  // em cassos do termo exister é uma incrementação na parte $inc
+  // em caso de não existir ira criar um documento novo com o termo no $setOnInsert
+  await collTopBusca.updateOne(
+    { termo },
+    {
+      $inc: { frequencia: 1 },
+      $setOnInsert: { termo }
+    },
+    {
+      upsert: true
+    }
+  );
 }
