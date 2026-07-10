a2709cb213ef1afad602e3e3b9e8de0d0a6d66a1
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-07-10 09:18:45 -0400

    ✨feat: adicionado serviço de sugestão
    adicionado service do serviço de sugestão

---
+++ back-end/src/modules/feedback/sugestão/sugestaoService.js
@@ -0,0 +1,24 @@
+import { detalheTermo} from "../../termo/termoModel.js";
+import { feedBackBD } from "../feedbackModel.js";
+
+// função princiapl do service de sugestão, com os parametros de query a info, e o tipo
+export async function verificaSugestao(query, tipo) {
+  // verifica se o query é nulo, ou ser é uma unica palavra já que não a collocações de uma unica palavra
+  // é usado split para separara as palavras, e length para verificar se é 1 unica;
+  if (!query || query.split(' ').length <= 1) return query.split(' ').length;
+
+  console.log(query.split(' ').length);
+  // armazena dos dados retornados da função detalheTermo, que sera usado de parametos o query
+  const existe = await detalheTermo(query);
+  /* verifica se a dados no constante existe, pois se houver,
+  significa que o query do usuario já existe, logo não pode ser armazenado */
+  if (existe != null || existe != []) {
+    // executa o armazenamento da sugestão do usuario, junto ao seu tipo de feedback
+    await feedBackBD(query, tipo);
+    // return com tipo booleano
+    return false;
+  }
+  // caso haja dados na constante existe, não sera armazenada
+  // return com tipo booleano
+  return true;
+}
