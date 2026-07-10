41be1aa67b431598c1d330d0dd480964ee990fd3
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-07-10 09:41:36 -0400

    ✨feat: adicionado nova rota para o serviço de pesquisa

--- back-end/src/routes/Routes.js
+++ back-end/src/routes/Routes.js
@@ -4,15 +4,17 @@ import { recomendacao, termoCompleto } from "../modules/termo/termoController.js
 import { favoritoArmazena, favoritoMostra } from "../modules/favorito/favoritoController.js";
 import { topBuscas } from "../modules/topBusca/topBuscaController.js";
 import { glossario } from "../modules/glossario/glossarioController.js";
+import { feedback } from "../modules/feedback/feedbackController.js";
 
 const router= express.Router();
 
-router.get('/search', pesquisa);
+router.get('/pesquisa', pesquisa);
 router.post('/favorito/:termo', favoritoArmazena);
 router.get('/favorito', favoritoMostra);
 router.get('/topBuscas', topBuscas);
 router.get('/detalheTermos/:termo', termoCompleto);
 router.get('/recomendacao/:termo', recomendacao);
 router.get('/glossario', glossario);
+router.get('/feedback/:tipo', feedback)
 
 export { router };
