c24db7ce9ff12d33783a1de371e9945bece72713
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-07-10 09:35:13 -0400

    💄style: melhorado os logs e adicionao comentario

--- back-end/src/modules/termo/termoController.js
+++ back-end/src/modules/termo/termoController.js
@@ -1,25 +1,35 @@
 import { detalheTermo } from "./termoModel.js";
 
+// função principal do serviço de termo completo
 export async function termoCompleto(req, res, next) {
+  // constante que armazena o parametro que está no url
   const termo = req.params.termo;
-  console.log(`completo`);
-  console.log(req.params.termo);
+  console.log(`no inicio da funcao de termoCompleto com ${termo} sendo requisitado `);
 
   try {
+    // constante que armazena o valor retornado pela função detalheTermo
+    // usa de parametro a constante termo
     const resultado = await detalheTermo(termo);
+    // envia resposta no formato json
     res.json(resultado);
   } catch (err) {
+    // em caso de erro no meio da execução
     next(err)
   }
 }
 
+// função principal do serviço de recomendações
 export async function recomendacao(req, res, next) {
+  // constante que armazena o parametro que está no url
   const termo = req.params.termo;
 
   try {
+    // constante que armazena o valor retornado pela função detalheTermo
     const resultado = await detalheTermo(termo);
+    // envia resposta no formato json, entretando, só envia a remissivas
     res.json(resultado.remissivas);
   } catch (err) {
+    // em caso de erro no meio da execução
     next(err)
   }
 }
