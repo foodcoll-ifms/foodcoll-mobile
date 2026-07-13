760fe7d6802e70cedd852568bc9a084bc1745026
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-07-13 16:14:02 -0400

    ♻️refactor: foi mudado o log e retirados partes do codigo

--- back-end/src/modules/pesquisa/pesquisaService.js
+++ back-end/src/modules/pesquisa/pesquisaService.js
@@ -1,26 +1,16 @@
-import { pesquisaDBC } from "./pesquisaModel.js";
-import { mostrarHistorico, insererirHistoricoDB } from "../../modules/historico/historicoModel.js";
+import { pesquisaDBC } from "../pesquisa/pesquisaModel.js";
+import { mostrarHistorico } from "../../modules/historico/historicoModel.js";
 
+// função principal de validação dos dados buscados. Parametro dado buscado
 export async function validaPesquisa(query) {
+  // if que compara o query se é vazio, se for mostra o historico
   if (!query) return mostrarHistorico();
 
-  console.log("oia eu  no service");
-  try {
-    const resultado = await pesquisaDBC(query);
-    console.log('penis');
+  console.log("no inicio da funcao validaPesquisa com o query não nulo");
 
-    return resultado;
-  } catch (err) {
-    const resultado = [];
-    console.log(err);
-    return resultado;
-  }
-
-}
-
-export async function guardaHistorico(query) {
-  if (!query) return 0;
-
-  await insererirHistoricoDB(query);
-  return 1;
+  // constante que armazena os dados retornado da função pesquisaDB
+  // usa o dado buscado como parametro
+  const resultado = await pesquisaDBC(query);
+  // retorna a constante resultado
+  return resultado;
 }
