e2a0a19e426567eb2251f0484326e189a5e49947
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-07-13 16:15:56 -0400

    ♻️refactor: Adicionado comentarios e logs melhorados
    Refactor verificaTop function to improve query validation and storage logic.

--- back-end/src/modules/topBusca/topBuscaService.js
+++ back-end/src/modules/topBusca/topBuscaService.js
@@ -1,19 +1,18 @@
 import { armazenaTopBuscas } from "./topBuscaModel.js";
 
-export async function vericivaTop(query, resposta) {
-  console.log("top");
-
+// função para verifar o dado que será (ou não) armazenado no top buscas
+// Usa de parametro o query, dado buscado, e resposta, a resposta o serviço Pesquisa
+export async function verificaTop(query, resposta) {
+  // if para ver se o query é pequeno ou nulo
   if (!query || query.length <= 2) {
     return 0
   };
-
-  console.log(`vai armazenar no top`);
-
-  for await (const doc of resposta[0].colocacao) {
-    if (doc.includes(query)) {
-      armazenaTopBuscas(query);
-      return 1;
-    }
+  console.log("no inico da função verificaTop");
+  // if para saber se o query está na resposta ou não
+  if (resposta[0].colocacao.includes(query) && query.includes(resposta[0].colocacao)) {
+    // armazeno a busca no top buscas
+    armazenaTopBuscas(query);
+    return 1;
   };
   return 0;
 }
