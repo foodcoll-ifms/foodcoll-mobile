98b97e09e1ae289f15efaaddc5ef045cc59b12e9
Eduardo Almeida Rodrigues <eduaralmeidarodri@gmail.com>
2026-07-13 16:09:01 -0400

    ♻️refactor: Uma unicia chamado no banco de dados e adicionaod comentarios
    Foi refatorado funções favoritas para combinar lógica de inserção e exclusão em inserirEdeletarFavorito. Atualizei mostarFavorito para simplificar a busca de favoritos.

--- back-end/src/modules/favorito/favoritoModel.js
+++ back-end/src/modules/favorito/favoritoModel.js
@@ -1,34 +1,51 @@
 import { collFavorito } from "../../config/db.js";
 
-export async function inserirFavorito(usuarioID, doc) {
-  await collFavorito.updateOne(
-    {
-      usuarioId: usuarioID,
-    },
-    {
-      $addToSet: {
-        colocacoesChave: doc,
-      },
-    },
-  );
-}
-
-export async function deleteFavorito(usuarioID, colocacoesChave) {
-  const resultado = await collFavorito.updateOne(
+//função de inserir na tabela de favorito a colocação favoritada
+export async function inserirEdeletarFavorito(usuarioID, colocacoesChave) {
+  /* executa um update no tabela de Favorito, usando de paremeto
+  o id do usuraio e acrescentando ao campo colocacoesChave o doc,
+  ou colocação favoritada*/
+  const resultado = await collFavorito.findOneAndUpdate(
     { usuarioId: usuarioID },
-    { $pull: { colocacoesChave: colocacoesChave } },
+    [
+      {
+        $set: {
+          colocacoesChave: {
+            $cond: [
+              { $in: [colocacoesChave, "$colocacoesChave"] },
+              {
+                $filter: {
+                  input: "$colocacoesChave",
+                  as: "item",
+                  cond: { $ne: ["$$item", colocacoesChave] }
+                }
+              },
+              {
+                $concatArrays: [
+                  "$colocacoesChave",
+                  [colocacoesChave]
+                ]
+              }
+            ]
+          }
+        }
+      }
+    ],
+    { returnDocument: "before" }
   );
   return resultado;
 }
 
-
+// função de mostrara o favorito com paramatro o id do usuario
 export async function mostarFavorito(usuarioID) {
-  const t = collFavorito.find({usuarioId: usuarioID}).project({_id: 0, usuarioId:0});
-  const resultado = [];
-  for await (const doc of t) {
-    doc.colocacoesChave.forEach(item => {
-      resultado.push(item);
-    })
-  }
-  return resultado;
+  // constante que armazena no todas as colocações buscada, com um tratamento
+  //pegando somente colocacoesChave
+    const doc = await collFavorito.findOne(
+        { usuarioId: usuarioID },
+        { projection: { _id: 0, colocacoesChave: 1 } }
+    );
+    // retorna somente as colocações favoritadas se existirem
+    // caso não existão ira retornar um array vazio
+    return doc?.colocacoesChave ?? [];
 }
+
